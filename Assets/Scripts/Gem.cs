2f5e4befaf0486e2960006e3d9b5c2e184ca6288
Vladimir Trofimov <trofimov57@mail.ru>
2021-11-14 22:31:13 +0200

    Swapping logic

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -9,6 +9,15 @@ public class Gem : MonoBehaviour
     [HideInInspector]
     public Board board;
 
+
+    private Vector2 firstTouchPosition;
+    private Vector2 finalTouchPosition;
+
+    private bool mousePressed;
+    private float swipeAngle = 0;
+
+    private Gem otherGem;
+
     void Start()
     {
         
@@ -17,12 +26,67 @@ public class Gem : MonoBehaviour
     
     void Update()
     {
-        
+        if(mousePressed && Input.GetMouseButtonUp(0))
+        {
+            mousePressed = false;
+
+            finalTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            CalculateAngle();
+        }
     }
 
     public void SetupGem(Vector2Int position, Board theBoard)
     {
         positionIndex = position;
         board = theBoard;
+    }
+
+    private void OnMouseDown()
+    {
+        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePressed = true;
+    }
+
+    private void CalculateAngle()
+    {
+        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x);
+        swipeAngle = swipeAngle * 180 / Mathf.PI;
+
+        if(Vector3.Distance(firstTouchPosition, finalTouchPosition) > .5f)
+        {
+            MovePieces();
+        }
+        
+    }
+
+    private void MovePieces()
+    {
+        if(swipeAngle < 45 && swipeAngle > -45 && positionIndex.x < board.width - 1)
+        {
+            otherGem = board.allGems[positionIndex.x + 1, positionIndex.y];
+            otherGem.positionIndex.x--;
+            positionIndex.x++;
+        }
+        else if (swipeAngle > 45 && swipeAngle <= 135 && positionIndex.y < board.height - 1)
+        {
+            otherGem = board.allGems[positionIndex.x, positionIndex.y + 1];
+            otherGem.positionIndex.y--;
+            positionIndex.y++;
+        }
+        else if (swipeAngle < -45 && swipeAngle >= -135 && positionIndex.y > 0)
+        {
+            otherGem = board.allGems[positionIndex.x, positionIndex.y - 1];
+            otherGem.positionIndex.y++;
+            positionIndex.y--;
+        }
+        else if (swipeAngle > 135 || swipeAngle < -45 && positionIndex.x > 0)
+        {
+            otherGem = board.allGems[positionIndex.x - 1, positionIndex.y];
+            otherGem.positionIndex.x++;
+            positionIndex.x--;
+        }
+
+        board.allGems[positionIndex.x, positionIndex.y] = this; // store current gem we move
+        board.allGems[otherGem.positionIndex.x, otherGem.positionIndex.y] = otherGem;
     }
 }
